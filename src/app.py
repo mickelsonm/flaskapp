8053ae371d41ecb7f0456c05dfce847309f20965
Matt Mickelson <mattmickelson@TruSignals-MacBook-Pro.local>
2016-01-26 17:59:33 -0600

    adds some jinja sauce

--- src/app.py
+++ src/app.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+from api import mod
 
 app = Flask(__name__)
-
-from api import mod
 app.register_blueprint(mod, url_prefix='/api')
 
-@app.route('/')
+# home / index
+@app.route("/")
+def index():
+    return render_template('home.html', title="Home")
+
+@app.route("/home")
 def home():
-	return 'Home'
+    return render_template('home.html', title="Home")
+
+# about
+@app.route("/about")
+def about():
+    return render_template('aboutus.html', title="About")
+
+# contact us
+@app.route("/contact")
+def contact():
+    return render_template('contactus.html', title="Contact Us")
+
+@app.route("/contacted", methods=["POST"])
+def contacted():
+	return render_template('contacted.html', title="Contacted")
 
 if __name__ == '__main__':
 	app.run(debug=False, host='0.0.0.0')
